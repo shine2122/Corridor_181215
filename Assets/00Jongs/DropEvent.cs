fa22c10c06d645e68658e4f7b3e3d7cdaf9ff171
shine2122 <44085013+shine2122@users.noreply.github.com>
2018-12-16 16:24:05 +0900

    지인 병원 복도와 불로 변경

---
+++ Assets/00Jongs/DropEvent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropEvent : MonoBehaviour {
+    public Rigidbody[] DropRigid;
+    public GameObject[] fireObj;
+    public ParticleSystem[] fires;
+	// Use this for initialization
+	void Start () {
+		
+	}
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Drop();
+        }
+    }
+    void Drop()
+    {
+        for (int i = 0; i < DropRigid.Length; i++)
+        {
+            fireObj[i].SetActive(true);
+            DropRigid[i].isKinematic = false;
+            fires[i].Play();
+        }
+    }
+}
---
+++ Assets/00Jongs/ExplosiveTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosiveTest : MonoBehaviour {
+
+    Transform MyTr;
+    public float power;
+    ParticleSystem explosion;
+    ParticleSystem[] fires;
+    public Rigidbody[] expObjs;
+    private void Start()
+    {
+        MyTr = this.transform;
+        explosion = MyTr.GetChild(6).GetComponent<ParticleSystem>();
+        fires = new ParticleSystem[6];
+        for (int i = 0; i < fires.Length; i++)
+        {
+            fires[i] = MyTr.GetChild(i).GetComponent<ParticleSystem>();
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Explosion();
+        }
+    }
+    void Explosion()
+    {
+        //폭발 효과 파티클 생성
+        explosion.Play();
+
+        //지정된 물체에 폭발력 부여
+        for (int i = 0; i < expObjs.Length; i++)
+        {
+            if (expObjs[i].isKinematic)
+                expObjs[i].isKinematic = false;
+            MyTr.LookAt(expObjs[i].transform);
+            Vector3 vect = MyTr.forward;
+            expObjs[i].AddForce(power * vect);
+        }
+
+        //폭발 직후 화제 시작
+        for (int i = 0; i < fires.Length; i++)
+        {
+            fires[i].Play();
+        }
+
+        //5초 후 드럼통 제거
+        //Destroy(gameObject, 5.0f);
+    }
+}
